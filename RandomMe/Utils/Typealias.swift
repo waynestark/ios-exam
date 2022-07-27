e78026172d8ef7352ce5b96bf77a167c92432a89
mpcarino <mps.carino101@gmail.com>
2022-07-28 01:58:02 +0800

    Create Typealias.swift

---
+++ RandomMe/Utils/Typealias.swift
@@ -0,0 +1,13 @@
+//
+//  Typealias.swift
+//  RandomMe
+//
+//  Created by Marwin Carino on 7/26/22.
+//
+
+import Foundation
+
+typealias S = R.string.localizable
+
+typealias VoidResult = () -> Void
+typealias ErrorResult = (Error) -> Void
